049cfd2743060ddbfbf6d65a221119805b031330
Vinicius <viniciusdosanjos63@gmail.com>
2024-05-17 20:37:56 -0300

    Async transition

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,17 +9,17 @@ namespace API.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
-    private readonly StoreContext _context;
+    private readonly IProductRepository _repo;
 
-    public ProductsController(StoreContext context)
+    public ProductsController(IProductRepository repository)
     {
-        _context = context;
+        _repo = repository;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-        var products = await _context.Products.ToListAsync();
+        var products = await _repo.GetProductsAsync();
 
         return Ok(products);
     }
@@ -26,6 +27,6 @@ public class ProductsController : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        return await _context.Products.FindAsync(id);
+        return await _repo.GetProductByIdAsync(id);
     }
 }
